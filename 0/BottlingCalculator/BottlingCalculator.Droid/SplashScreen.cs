bb0c6917757c6387e05cd0b047c48c739ca02906
Iain Smith <b099l3@gmail.com>
2018-07-30 23:58:14 +0100

    Mvx0 - Droid Added

---
+++ 0/BottlingCalculator/BottlingCalculator.Droid/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using BottlingCalculator.Core;
+using MvvmCross.Platforms.Android.Core;
+using MvvmCross.Platforms.Android.Views;
+
+namespace BottlingCalculator.Droid
+{
+    [Activity(
+        Label = "BottlingCalculator"
+        , MainLauncher = true
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity<MvxAndroidSetup<App>, App>
+    {
+        public SplashScreen()
+            : base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
---
+++ 0/BottlingCalculator/BottlingCalculator.Droid/Views/HomeView.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace BottlingCalculator.Droid.Views
+{
+    [MvxActivityPresentation]
+    [Activity(Label = "View for HomeViewModel")]
+    public class HomeView : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.HomeView);
+        }
+    }
+}
